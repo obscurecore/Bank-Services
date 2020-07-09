5d377a70138806fa18081e6b1281c687f2065969
Potapov Ruslan <pruslang@gmail.com>
2020-07-09 16:40:17 +0300

    add exception

---
+++ stonks-service/src/main/java/bank/service/stonksservice/payload/ErrorResponse.java
@@ -0,0 +1,19 @@
+package bank.service.stonksservice.payload;
+
+public class ErrorResponse {
+
+    private String message;
+
+    public ErrorResponse(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
