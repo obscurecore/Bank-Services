66f2acf0c7f26ba3e0c0cfc49786e85b1f7a6198
Potapov Ruslan <pruslang@gmail.com>
2020-07-09 17:00:55 +0300

    add dto for user-service

---
+++ user-service/src/main/java/bank/service/userservice/model/Stonk.java
@@ -0,0 +1,28 @@
+package bank.service.userservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Data
+@Builder
+@AllArgsConstructor
+@Document(collection = "buckets")
+public class Stonk {
+
+    @Id
+    private String id;
+
+    @NotBlank
+    @Size(max = 10)
+    private String title;
+
+    private String description;
+    private int personalNumber;
+    private String imageLink;
+
+}
