a0b0b5557e06b884be47799b93d5b68994786212
Potapov Ruslan <pruslang@gmail.com>
2020-07-09 17:41:10 +0300

    add data

---
+++ fine-card-service/src/main/java/bank/service/finecardservice/client/dto/FineDTO.java
@@ -0,0 +1,17 @@
+package bank.service.finecardservice.client.dto;
+
+import bank.service.finecardservice.entity.FineCard;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+public class FineDTO {
+    private String id;
+    private BigDecimal amount;
+    private FineCard.FineType fineType;
+    private Date dueDate;
+}
