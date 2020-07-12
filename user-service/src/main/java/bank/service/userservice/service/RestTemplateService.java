48916261b06ccf8402ff70bfde36881c93473606
Potapov Ruslan <pruslang@gmail.com>
2020-07-12 20:24:10 +0300

    add rest template

---
+++ user-service/src/main/java/bank/service/userservice/service/RestTemplateService.java
@@ -0,0 +1,35 @@
+package bank.service.userservice.service;
+
+import com.netflix.discovery.EurekaClient;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.sun.tools.sjavac.Log;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * The type Rest template service.
+ *
+ * @author Ruslan Potapov
+ */
+@Service
+@RequiredArgsConstructor
+public class RestTemplateService {
+    private final RestTemplate restTemplate;
+
+    @HystrixCommand(fallbackMethod = "failed")
+    public String data() {
+        String response = restTemplate.getForObject("http://stonks-service/data", String.class);
+        Log.log(Log.Level.INFO, response);
+        return response;
+    }
+
+    public String failed() {
+        String error = "Service is not available now. Please try later";
+        Log.log(Log.Level.INFO, error);
+        return error;
+    }
+
+}
