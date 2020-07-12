381bf523d0759fbd54a9bed60272889aa6c2d950
Potapov Ruslan <pruslang@gmail.com>
2020-07-12 20:25:37 +0300

    #refactor

--- user-service/src/main/java/bank/service/userservice/controller/UserController.java
+++ user-service/src/main/java/bank/service/userservice/controller/UserController.java
@@ -3,8 +3,8 @@ package bank.service.userservice.controller;
 
 import bank.service.userservice.model.Stonk;
 import bank.service.userservice.service.ServiceFeignClient;
-import bank.service.userservice.service.TestService;
-import org.springframework.beans.factory.annotation.Autowired;
+import bank.service.userservice.service.RestTemplateService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.core.env.Environment;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,40 +16,47 @@ import java.util.List;
 /**
  * Communicate with stonks-service and will be gotten data from DB through stonks-service.
  * So did't directly communicate with DB
+ *
+ * @author Ruslan Potapov
  */
 @RestController
 @RequestMapping("/")
+@RequiredArgsConstructor
 public class UserController {
 
-    //Reading environment variables
-    @Autowired
-    private Environment env;
-    @Autowired
-    private TestService service;
-    @Autowired
-    ServiceFeignClient serviceFeignClient;
+    private final Environment env;
+    private final RestTemplateService service;
 
+    /**
+     * This is useful for debugging
+     * When having multiple instance of gallery service running at different ports.
+     * We load balance among them, and display which instance received the request.
+     *
+     * @return the string
+     */
     @RequestMapping("/")
     public String home() {
-        // This is useful for debugging
-        // When having multiple instance of gallery service running at different ports.
-        // We load balance among them, and display which instance received the request.
         return "Hello from User-Service running at port: " + env.getProperty("local.server.port");
     }
 
-    // Using Feign Client
+    /**
+     * Using Feign Client
+     *
+     * @param model the model
+     * @return the data
+     */
     @RequestMapping(path = "/getAllDataFromGalleryService")
     public List<Stonk> getData(Model model) {
-        //List<Bucket> list = ServiceFeignClient.FeignHolder.create().getAllEmployeesList();
-        List<Stonk> list = serviceFeignClient.getAllEmployeesList();
-//        model.addAttribute("employees", list);
-//        return "resultlist-employees";
-        return list;
+        return ServiceFeignClient.FeignHolder.create().getAllEmployeesList();
     }
 
-    // Using RestTemplate
+    /**
+     * Using RestTemplate
+     *
+     * @return the string
+     */
     @GetMapping("/data")
-    public String data(){
+    public String data() {
         return service.data();
     }
 
