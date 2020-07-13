70a205d39fa44e73839296fce187d8fdddba058f
Potapov Ruslan <pruslang@gmail.com>
2020-07-13 20:03:37 +0300

    #refactor

--- Benchmark/src/main/java/bank/service/benchmark/ClientFactory.java
+++ Benchmark/src/main/java/bank/service/benchmark/ClientFactory.java
@@ -12,36 +12,37 @@ import java.util.Map;
 
 /**
  * The type Client factory.
+ *
  * @author Ruslan Potapov
  */
 public class ClientFactory {
 
-  @SneakyThrows
-  public static HttpClientRequest<ByteBuf, ByteBuf> createClient(String address, boolean ignoreSSl, Map<String,String> headers) {
-    final URI uri = URI.create(address.startsWith("http") ? address :  "http://"+address);
-    final int port  = uri.getPort() < 0 ? 80 : uri.getPort();
+    @SneakyThrows
+    public static HttpClientRequest<ByteBuf, ByteBuf> createClient(String address, boolean ignoreSSl, Map<String, String> headers) {
+        final URI uri = URI.create(address.startsWith("http") ? address : "http://" + address);
+        final int port = uri.getPort() < 0 ? 80 : uri.getPort();
 
-    HttpClient<ByteBuf, ByteBuf> httpClient = HttpClient.newClient(uri.getHost(), port);
-    if(uri.getScheme().equalsIgnoreCase("https")) {
-      if (ignoreSSl)
-        httpClient = httpClient.unsafeSecure();
-      else
-        httpClient = httpClient.secure(defaultSSLEngineForClient(uri.getHost(), uri.getPort()));
+        HttpClient<ByteBuf, ByteBuf> httpClient = HttpClient.newClient(uri.getHost(), port);
+        if (uri.getScheme().equalsIgnoreCase("https")) {
+          if (ignoreSSl)
+                httpClient = httpClient.unsafeSecure();
+            else
+                httpClient = httpClient.secure(defaultSSLEngineForClient(uri.getHost(), uri.getPort()));
+        }
+        HttpClientRequest<ByteBuf, ByteBuf> resultedClient = httpClient.createGet(uri.getPath() + "?" + uri.getQuery());
+        if (headers != null) {
+            for (Map.Entry<String, String> header : headers.entrySet()) {
+                resultedClient = resultedClient.addHeader(header.getKey(), header.getValue());
+            }
+        }
+        return resultedClient;
     }
-     HttpClientRequest<ByteBuf, ByteBuf> resultedClient = httpClient.createGet(uri.getPath() + "?" + uri.getQuery());
-    if(headers != null) {
-      for(Map.Entry<String,String> header: headers.entrySet()) {
-        resultedClient = resultedClient.addHeader(header.getKey(), header.getValue());
-      }
-    }
-    return resultedClient;
-  }
 
-  @SneakyThrows
-  private static SSLEngine defaultSSLEngineForClient(String host, int port) {
-    SSLContext sslCtx = SSLContext.getDefault();
-    SSLEngine sslEngine = sslCtx.createSSLEngine(host, port);
-    sslEngine.setUseClientMode(true);
-    return sslEngine;
-  }
+    @SneakyThrows
+    private static SSLEngine defaultSSLEngineForClient(String host, int port) {
+        SSLContext sslCtx = SSLContext.getDefault();
+        SSLEngine sslEngine = sslCtx.createSSLEngine(host, port);
+        sslEngine.setUseClientMode(true);
+        return sslEngine;
+    }
 }
