b0607fed16636edba82b287344f0fc4307fb036f
Potapov Ruslan <pruslang@gmail.com>
2020-07-13 20:07:54 +0300

    #refactor

--- Benchmark/src/main/java/bank/service/benchmark/logger/ColloredHelpFormatter.java
+++ Benchmark/src/main/java/bank/service/benchmark/logger/ColloredHelpFormatter.java
@@ -12,16 +12,16 @@ import java.util.Map;
  */
 public class ColloredHelpFormatter extends BuiltinHelpFormatter {
 
-  public ColloredHelpFormatter() {
-    super(80, 2);
-  }
+    public ColloredHelpFormatter() {
+        super(80, 2);
+    }
 
-  public ColloredHelpFormatter(int desiredOverallWidth, int desiredColumnSeparatorWidth) {
-    super(desiredOverallWidth, desiredColumnSeparatorWidth);
-  }
+    public ColloredHelpFormatter(int desiredOverallWidth, int desiredColumnSeparatorWidth) {
+      super(desiredOverallWidth, desiredColumnSeparatorWidth);
+    }
 
-  @Override
-  public String format(Map<String, ? extends OptionDescriptor> options) {
-    return ColoredLogger.GREEN + super.format(options);
-  }
+    @Override
+    public String format(Map<String, ? extends OptionDescriptor> options) {
+        return ColoredLogger.GREEN + super.format(options);
+    }
 }
