441d08b8cb08355eb7afac93d31c2a18e2b8320e
Potapov Ruslan <pruslang@gmail.com>
2020-07-14 18:48:53 +0300

    add service

---
+++ bucket-service/src/main/java/bunk/service/bucketservice/service/CheckOrderService.java
@@ -0,0 +1,28 @@
+package bunk.service.bucketservice.service;
+
+import bunk.service.bucketservice.model.Order;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Processor;
+import org.springframework.integration.annotation.MessageEndpoint;
+import org.springframework.messaging.handler.annotation.SendTo;
+
+@EnableBinding(Processor.class)
+@MessageEndpoint
+public class CheckOrderService {
+
+    @StreamListener(Processor.INPUT)
+    @SendTo(Processor.OUTPUT)
+    Order transform(Order order) {
+
+        Order checkedOrder = new Order(order.getId(),
+                order.getType(),
+                order.getName(),
+                "checked");
+
+        System.out.println(checkedOrder);
+
+        return checkedOrder;
+    }
+
+}
