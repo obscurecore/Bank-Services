7397e180ece48500cf36f83250b3c339569bf66e
Potapov Ruslan <pruslang@gmail.com>
2020-07-14 14:59:56 +0300

    set up ribbon config

--- user-service/src/main/java/bank/service/userservice/UserServiceApplication.java
+++ user-service/src/main/java/bank/service/userservice/UserServiceApplication.java
@@ -1,5 +1,6 @@
 package bank.service.userservice;
 
+import bank.service.userservice.config.RibbonConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
@@ -7,6 +8,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -22,6 +24,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableCircuitBreaker
 @EnableHystrixDashboard
 @EnableEurekaClient
+@RibbonClient(name = "stonks-service", configuration = RibbonConfiguration.class)
 public class UserServiceApplication {
 
     public static void main(String[] args) {
