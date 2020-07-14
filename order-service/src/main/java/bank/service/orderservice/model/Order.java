e99458b9dfa1f92a40551453340f5186768325d7
Potapov Ruslan <pruslang@gmail.com>
2020-07-14 18:28:05 +0300

    add module

---
+++ order-service/src/main/java/bank/service/orderservice/model/Order.java
@@ -0,0 +1,14 @@
+package bank.service.orderservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Order {
+
+    private final int id;
+    private final String type;
+    private final String name;
+
+}
