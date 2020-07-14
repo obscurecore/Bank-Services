8f35f1936d98de19ee92e6763d509050bd776e3e
Potapov Ruslan <pruslang@gmail.com>
2020-07-14 13:18:41 +0300

    add application

--- config-server/src/main/java/bank/service/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/bank/service/configserver/ConfigServerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableConfigServer
-//@EnableEurekaClient
+@EnableEurekaClient
 @SpringBootApplication
 public class ConfigServerApplication {
 
