a85dfb930612d0e0e7fdf2acb61979fb8eb56100
Potapov Ruslan <pruslang@gmail.com>
2020-07-19 13:59:04 +0300

    fix package sun

--- user-service/src/main/java/bank/service/userservice/service/RestTemplateService.java
+++ user-service/src/main/java/bank/service/userservice/service/RestTemplateService.java
@@ -1,7 +1,6 @@
 package bank.service.userservice.service;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-import com.sun.tools.sjavac.Log;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -19,13 +18,13 @@ public class RestTemplateService {
     @HystrixCommand(fallbackMethod = "failed")
     public String data() {
         String response = restTemplate.getForObject("http://stonks-service/data", String.class);
-        Log.log(Log.Level.INFO, response);
+        //Log.log(Log.Level.INFO, response);
         return response;
     }
 
     public String failed() {
         String error = "Service is not available now. Please try later";
-        Log.log(Log.Level.INFO, error);
+       // Log.log(Log.Level.INFO, error);
         return error;
     }
 
