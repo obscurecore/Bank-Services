f90aee861d9e7a7c3af672f9a2387e788295299c
Potapov Ruslan <pruslang@gmail.com>
2020-07-19 04:00:57 +0300

    add fines cards client

---
+++ user-service/src/main/java/bank/service/userservice/client/FinesCardClient.java
@@ -0,0 +1,28 @@
+package bank.service.userservice.client;
+
+import bank.service.userservice.client.dto.FineCard;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+
+import java.math.BigDecimal;
+
+@Service
+public class FinesCardClient implements CardClient<FineCard> {
+
+    private final BaseCardClient cardClient;
+
+    public FinesCardClient(@Value("${hub.finesURL}") String url) {
+        this.cardClient = new BaseCardClient(url);
+    }
+
+    public Flux<FineCard> getCards(String userId,
+                                   BigDecimal longitude,
+                                   BigDecimal latitude,
+                                   Long currentDate) {
+        return cardClient.getCards(userId, longitude, latitude, currentDate, MediaType.APPLICATION_STREAM_JSON)
+                .flatMapMany(res -> res.bodyToFlux(FineCard.class));
+    }
+}
+
