6ed8fef6b2a0d0dbbad953df23f9fd6ca82b4d02
Ruslan Potapov <pruslang@gmail.com>
2020-10-02 13:07:28 +0300

    dto

---
+++ bucket-service/src/main/java/bank/service/bucketservice/message/MyMessage.java
@@ -0,0 +1,15 @@
+package bank.service.bucketservice.message;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MyMessage implements Serializable {
+
+    private String message;
+}
